c3db5a145039e0e0d9aec7147d51250653af057b
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2022-04-05 14:31:46 +0300

    Removed useless tags

--- transaction.go
+++ transaction.go
@@ -19,19 +19,19 @@ package tcabci_read_go_client
 import "time"
 
 type Transaction struct {
-	ID            uint64    `db:"id" json:"id"`
-	BlockID       uint64    `db:"block_id" json:"block_id"`
-	Height        uint64    `db:"height" json:"height"`
-	Identifier    string    `db:"identifier" json:"identifier"`
-	Version       uint      `db:"version" json:"version"`
-	Typ           string    `db:"typ" json:"typ"`
-	SenderAddr    string    `db:"sender_addr" json:"sender_addr"`
-	RecipientAddr string    `db:"recipient_addr" json:"recipient_addr"`
-	Data          Bytea     `db:"data" json:"data"`
-	Sign          Bytea     `db:"sign" json:"sign"`
-	Fee           uint64    `db:"fee" json:"fee"`
-	Hash          string    `db:"hash" json:"hash"`
-	InsertedAt    time.Time `db:"inserted_at" json:"inserted_at"`
+	ID            uint64    `json:"id"`
+	BlockID       uint64    `json:"block_id"`
+	Height        uint64    `json:"height"`
+	Identifier    string    `json:"identifier"`
+	Version       uint      `json:"version"`
+	Typ           string    `json:"typ"`
+	SenderAddr    string    `json:"sender_addr"`
+	RecipientAddr string    `json:"recipient_addr"`
+	Data          Bytea     `json:"data"`
+	Sign          Bytea     `json:"sign"`
+	Fee           uint64    `json:"fee"`
+	Hash          string    `json:"hash"`
+	InsertedAt    time.Time `json:"inserted_at"`
 }
 
 type Bytea struct {
