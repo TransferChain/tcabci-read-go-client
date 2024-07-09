78706ed6afcc5d16758a61b8f14971da77f6bfc6
Abdulkadir DİLSİZ <akdilsiz@tecpor.com>
2024-07-09 12:14:32 +0300

    Version bumps

--- client.go
+++ client.go
@@ -105,7 +105,7 @@ type sendMsg struct {
 // NewClient make ws client
 func NewClient(address string, wsAddress string) (Client, error) {
 	c := new(client)
-	c.version = "v1.2.7"
+	c.version = "v1.2.8"
 	c.address = address
 	c.wsAddress = wsAddress
 
