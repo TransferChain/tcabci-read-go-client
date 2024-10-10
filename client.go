484f7f743887723c32fef8d8b544e7c64063824f
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2024-10-11 02:16:17 +0300

    Version bumps

--- client.go
+++ client.go
@@ -135,7 +135,7 @@ func newClient(ctx context.Context, address string, wsAddress string) (Client, e
 
 	c := &client{
 		ctx:                 ctx,
-		version:             "v1.2.12",
+		version:             "v1.2.13",
 		address:             address,
 		wsAddress:           wsAddress,
 		url:                 aURL,
