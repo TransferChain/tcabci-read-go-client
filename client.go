356c438c2e9a9ef04eb49b55fc8f35ac5ecefa5c
Abdulkadir DILSIZ <abdulkadir.dilsiz@transferchain.io>
2026-03-15 00:17:30 +0300

    Version bumps

--- client.go
+++ client.go
@@ -164,7 +164,7 @@ func newClient(ctx context.Context, address string, wsAddress string, chainName,
 
 	c := &client{
 		ctx:                 ctx,
-		version:             "1.6.11",
+		version:             "1.6.12",
 		lgr:                 NewLogger(ctx),
 		address:             address,
 		wsAddress:           wsAddress,
