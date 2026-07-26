80cbb2fd055a6742406208d31a48cab2a757f0d3
Abdulkadir D <akdilsiz@tecpor.com>
2026-07-26 10:07:35 +0300

    Version bumps

--- client.go
+++ client.go
@@ -206,7 +206,7 @@ func newClient(ctx context.Context, address string, wsAddress string, chainName,
 
 	c := &client{
 		ctx:                  ctx,
-		version:              "1.6.32",
+		version:              "1.6.33",
 		lgr:                  NewLogger(ctx),
 		mode:                 Subscription,
 		address:              address,
